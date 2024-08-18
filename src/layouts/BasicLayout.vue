d32051f1809d5dd6a43ebf685654c288d6ee88f9
bushiqbw <1036279449@qq.com>
2024-08-18 14:36:57 +0800

    全局布局及用户状态存储

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/bushiqbw/" target="_blank">
+          阿强哥的GitHub
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  margin-bottom: 16px;
+  box-shadow: #eee 1px 1px 5px;
+}
+
+#basicLayout .content {
+  max-width: 1200px;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0 auto 28px;
+  padding: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+}
+
+.footer {
+  padding: 16px;
+  text-align: center;
+  background: #efefef;
+}
+</style>
